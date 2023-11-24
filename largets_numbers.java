a3499ca7b835cbcee905c7c25c2bfd2027ad9724
cinex1996 <34630978+cinex1996@users.noreply.github.com>
2023-11-24 13:58:16 +0100

    Update largets_numbers.java

--- largets_numbers.java
+++ largets_numbers.java
@@ -1,49 +1,27 @@
-import java.util.InputMismatchException;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
+public class largets_numbers {
+    void largest_number() {
+
         boolean value = true;
-        result m1 = new result();
-        rok_przystępny year = new rok_przystępny();
-        largets_numbers number=new largets_numbers();
-        Scanner answer = new Scanner(System.in);
-        int choice = 0;
         while (value) {
+            int[] tablle = new int[3];
+            int[] new_table=new int[3];
+            Scanner scanner = new Scanner(System.in);
 
-                System.out.println("Welcome in superprogram");
-                System.out.println("1.Run program which is about result finish score");
-                System.out.println("2.Run program which is about year accesible");
-                System.out.println("3.Run program which check larggest number");
-                System.out.println("4.Sort numbers");
-                System.out.println("5.Exit program");
-                System.out.println("Give your choice");
-            try {
-                choice=answer.nextInt();
-                if (choice == 1) {
-                    m1.result();
-                } else if (choice == 2) {
-                    year.year();
-                }
-                else if(choice==3) {
-                        number.largest_number();
-                    }
-                 else if (choice == 5) {
-                    value = false;
-                    System.out.println("The program finish work");
-                }
-            } catch (InputMismatchException e)
-            {
-
-                System.out.println("You can't write letters only numbers!!");
-                answer.next();
-
+            System.out.println("Enter 3 numbers:");
+            for (int i = 0; i < tablle.length; i++) {
+                System.out.print("Enter number " + (i + 1) + ": ");
+                tablle[i] = scanner.nextInt();
             }
-
-
-
+            int maxlength1 = tablle[0];
+            for (int i = 1; i < tablle.length; i++) {
+                if (tablle[i] > maxlength1) {
+                    maxlength1 = tablle[i];
+                }
+            }
+            System.out.println(maxlength1);
 
 
         }
